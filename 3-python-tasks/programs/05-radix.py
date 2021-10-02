146a664e5ccebd2c7ec3c018f6813486ea7f30ce
Dineshkumar Sundaram <dineshh912@gmail.com>
2021-10-02 22:49:56 +0530

    added

--- 3-python-tasks/programs/05-radix.py
+++ 3-python-tasks/programs/05-radix.py
@@ -2,6 +2,15 @@
 #RETURN Wild Number [string, base]
 #string is encoding of number in base, base is integer
 def make_number(decimal, base):
+    """
+        This function will convert decimal number in to base number
+        -- input
+            decimal - deciaml input value which needed conversion
+            base - base value
+
+        -- output
+            wild number which contain converted decimal and new base
+    """
     base_num = ""
     while decimal > 0:
         dig = int(decimal%base)
@@ -12,33 +21,58 @@ def make_number(decimal, base):
 
     return [base_num[::-1], base]
 
-#INPUT Wild number 
-#RETURN new wild number in new base
+
 def convert(number, base):
+    """
+        This function convert one base to another base
+        -- input
+            wild number generated using make_number function
+        -- ouput
+            newly converted number
+    """
     wild_number =  number[0]
     wild_number_base = number[1]
     conversion = int(wild_number, wild_number_base)
-    
+
     return [conversion, base]
 
-#INPUT two wild numbers
-#RETURN product as a (possibly new) base
+
 def mul_(number1, number2, base):
-    number_1 = conversion = int(number1[0], number1[1])
-    number_2 = conversion = int(number2[0], number2[1])
+    """
+        This function will multiply two base number
+        -- input
+            
+    """
+    number_1 = convert(number1, 10)
+    number_2 = convert(number2, 10)
+    if base == 10:
+        result = number_1[0] * number_2[0]
+    elif base == 2:
+        mul = number_1[0] * number_2[0]
+        result = make_number(mul,2)[0]
 
-    print(number1*number2)
+    return [result, base]
 
 
-#INPUT two wild numbers
-# #RETURN sum as a (possibly new) base
 def add_(number1, number2, base):
-    number_1 = convert(number1, base)[0]
-    number_2 = convert(number2, base)[0]
+    """
+        This function will multiply two base number
+        -- input
+            number 1 - wild number with it's base
+            number 2 - wild number with it's base
+            base - new base
+        -- output
+            sum of number with new base  
+    """
+    number_1 = convert(number1, 10)
+    number_2 = convert(number2, 10)
+    if base == 10:
+        result = number_1[0] + number_2[0]
+    elif base == 2:
+        add = number_1[0] + number_2[0]
+        result = make_number(add,2)[0]
 
-    add = number_1 + number_2
-
-    return [add, base]
+    return [result, base]
 
 if __name__ == "__main__":
     n1, n2 = 5, 4
@@ -52,5 +86,4 @@ if __name__ == "__main__":
     print(convert(add_(x1,y1, base2),base10))
     print(mul_(x1, y1, base2))
     print(convert(mul_(x1,y1, base2),base10))
-
 
