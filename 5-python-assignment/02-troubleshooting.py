db11242a59f83b9e12466f1c59b7d31af2b2fa0e
dineshh <dinesh.kumar@avmsolutionsuk.com>
2021-10-07 13:28:52 +0100

    assignment4,5

---
+++ 5-python-assignment/02-troubleshooting.py
@@ -0,0 +1,10 @@
+zoo = ['Lion', 'Zebra', 'Giraffe', 'Hippo'] 
+for animal in zoo:
+    if animal == "Lion":
+        print("Alex the " + animal)
+    elif animal == 'Zebra':
+        print ("Marty the " + animal)    
+    elif animal == 'Giraffe':
+        print ("Melman the " + animal)
+    elif animal == 'Hippo':
+        print ("Gloria the " + animal)
