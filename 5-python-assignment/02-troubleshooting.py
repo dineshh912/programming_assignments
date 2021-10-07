6a4512b2c4ba85e9139e0382cc9a5e197d61b283
dineshh <dinesh.kumar@avmsolutionsuk.com>
2021-10-07 14:39:36 +0100

    updated4,5

--- 5-python-assignment/02-troubleshooting.py
+++ 5-python-assignment/02-troubleshooting.py
@@ -1,10 +1,35 @@
-zoo = ['Lion', 'Zebra', 'Giraffe', 'Hippo'] 
+zoo = ['Lion', 'Zebra', 'Giraffe', 'Hippo']
 for animal in zoo:
+    # if animal == "Lion': XXXXXXXXXX 
+    '''
+    Above line conditon value surrounded by both single & Double quotes which causes error
+    we sould not add anthing after colon(:) this will cause indentation problem.'''
+    
     if animal == "Lion":
+        # print "Alex the " + Animal
+        '''
+        In for loop iterator is animal which is all lower case in print statement 
+        A is upper case. and print statement should surround by () brackets. unless it's python version 2.X 
+        '''
         print("Alex the " + animal)
-    elif animal == 'Zebra':
-        print ("Marty the " + animal)    
-    elif animal == 'Giraffe':
-        print ("Melman the " + animal)
-    elif animal == 'Hippo':
-        print ("Gloria the " + animal)
+    # elif animal == 'Zebra': XXXXXXXXXX
+    # we sould not add anthing after colon(:) this will cause indentation problem.
+    elif animal == "Zebra":
+        # print "Marty the " + Animal
+        '''
+        In for loop iterator is animal which is all lower case in print statement 
+        A is upper case. and print statement should surround by () brackets. unless it's python version 2.X 
+        '''
+        print("Marty the " + animal)
+    #elif animal == 'Giraffe': XXXXXXXXXX
+    # we sould not add anthing after colon(:) this will cause indentation problem.
+    elif animal == "Giraffe":
+        # print "Melman the " + animals
+        # print statement should surround by () brackets. unless it's python version 2.X 
+        print("Melman the " + animal)
+    # elif animal == 'Hippo': XXXXXXXXXX
+     # we sould not add anthing after colon(:) this will cause indentation problem.
+    elif animal == "Hippo":
+        #print "Gloria the " + animal
+         # print statement should surround by () brackets. unless it's python version 2.X 
+        print("Gloria the " + animal)
--- 5-python-assignment/03-implementation.py
+++ 5-python-assignment/03-implementation.py
@@ -1,14 +1,14 @@
-# Import the Random Module 
-import random  
+# Import the Random Module
+import random 
 # Code Name Lists 
 alpha = ['Crimson', 'Phantom', 'Zephyr', 'Palisade', 'Skyfall'] 
 omega = ['Whirlwind', 'Gatecrasher', 'Iceberg', 'Zealot', 'Element']  
-# 1.CREATE FOR LOOP HERE # HINT: You will have a nested For loop.  
 
+# 1.CREATE FOR LOOP HERE 
+# HINT: You will have a nested For loop. 
 for _ in alpha:
     for _ in omega:
-        # 2. The Random Code Names will print here.
+
+# 2. The Random Code Names will print here.  
+# HINT: Remember your indentions.  
         print ("Operation: " + random.choice(alpha) + " " + random.choice(omega))
-        
-# HINT: Remember your indentions. 
- 
