aa0e92b04a97c8add9c0a0f90d18b1d0e9667948
dineshh <dinesh.kumar@avmsolutionsuk.com>
2021-10-27 11:25:14 +0100

    added-viz

--- 12-objectively-funcy-js/main.js
+++ 12-objectively-funcy-js/main.js
@@ -1,28 +1,41 @@
 // Your code here.
 
 // Get input person object and return only firstname
-const getFirstName = person => person.firstName;
+const getFirstName = function(person) {
+
+    return person["firstName"];
+}
 
 // Get input person object and return only lastname
-const getLastName = person => person.lastName;
+const getLastName = function(person) {
+
+    return person["lastName"];
+}
 
 // Get input person and concat both firstname and lastname
-const getFullName = person => `${person.firstName} ${person.lastName}`;
+const getFullName = function(person) {
+    return `${person["firstName"]} ${person["lastName"]}`;
+}
 
 // Get person object and new firstname, need to replace person object firstname with new name
-const setFirstName = (person, newFirstName) => person.firstName = newFirstName;
+const setFirstName = function(person, name) {
+    person["firstName"] = name;
+}
 
 // Get person object and new Age, need to replace person object Age with new age
-const setAge = (person, newAge) => person.age = newAge;
+const setAge = function(person, age) {
+    person["age"] = age;
+}
 
 // Get person object, if age contains in the person increase age by 1 or return 1.
-const giveBirthday = person => {
+const giveBirthday = function(person) {
 
     if ("age" in person) {
-        person.age += 1;
+        person["age"]++;
     } else {
-        person.age = 1;
+        person["age"] = 1;
     }
+
 }
 
 // Set married property as true, and fill spouseName based on First name and last name
