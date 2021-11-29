c333087958a99bd75760a4588a3d092267cf0241
dineshh <dinesh.kumar@avmsolutionsuk.com>
2021-11-29 14:16:07 +0000

    python-heat

--- 28-97153-python/heatDiff.py
+++ 28-97153-python/heatDiff.py
@@ -4,70 +4,70 @@ import matplotlib.pyplot as plt # Visualization library
 import matplotlib.colors as mcolors
 import time # for timing
 
-# laminar metal
-    # created halo cells
-    grid = np.array([[temp * 1.00 if i == 0 else 0.00 for i in range(temp + 2)] for j in range(temp + 2)])
-
-    num_iter = 0
-    MAX_ITER = 3000
-    while num_iter < MAX_ITER:
-        old_grid = np.copy(grid)
-        grid = distribute_heat(old_grid)
-        if equals(old_grid, grid):
-            break
-        num_iter += 1
-
-    show_grid = np.array([[grid[i][j] for j in range(1, len(grid) - 1)] for i in range(1, len(grid) - 1)])
-
-
-    #end time
-    end = time.time()
-
-    time_taken = end - start
-    print("Time taken ", time_taken, "seconds")
-
-    plot_grid(show_grid)
-
 
 def main(temp):
     try:
-        pass
+        # Creating halo cells & create as grid
+        state = np.array([[temp * 1.00 if i == 0 else 0.00 for i in range(temp + 2)] for j in range(temp + 2)])
+
+        num_iteration = 0
+        max_iteration_limit = 3000
+        while num_iteration < max_iteration_limit:
+            state_1 = np.copy(state)
+            # calculating temp
+            state = calculate_temp(state_1)
+            if equals(state_1, state):
+                break
+            num_iteration += 1
+       
+        return state
+
     except Exception as e:
         print(e)
 
 
-def distribute_heat(old_grid):
-    new_grid = np.copy(old_grid)
-    for i in range(1,len(new_grid) - 1):
-        for j in range(1,len(grid) - 1):
-            new_grid[i, j] = 0.25*(old_grid[i - 1, j] + old_grid[i + 1, j] + old_grid[i, j - 1] + old_grid[i, j+1])
-    return new_grid
+def calculate_temp(state_1):
+    state_2 = np.copy(state_1)
+    for i in range(1,len(state_2) - 1):
+        for j in range(1,len(state_1) - 1):
+            state_2[i, j] = 0.25*(state_1[i - 1, j] + state_1[i + 1, j] + state_1[i, j - 1] + state_1[i, j+1])
+    return state_2
 
 
-def equals(old_grid, new_grid):
-    for i in range(1, len(old_grid) - 1):
-        for j in range(1, len(old_grid) - 1):
-            if old_grid[i, j] != new_grid[i, j]:
+def equals(state_1, state):
+    for i in range(1, len(state_1) - 1):
+        for j in range(1, len(state_1) - 1):
+            if state_1[i, j] != state[i, j]:
                 return False
     return True
 
 
-def plot_grid(grid):
+def plot_state(state):
     fig, ax = plt.subplots()
-    y = np.array([[_ for __ in range(len(grid))] for _ in range(len(grid))])
-    x = np.array([[__ for __ in range(len(grid))] for _ in range(len(grid))])
-    C = grid
+    y = np.array([[_ for __ in range(len(state))] for _ in range(len(state))]) # creating x axis
+    x = np.array([[__ for __ in range(len(state))] for _ in range(len(state))]) # creating y axis
+    C = state
     cmap = mcolors.ListedColormap(['darkblue', 'blue', 'aqua', 'lawngreen', 'yellow', 'orange', 'red', 'darkred'])
     ax.scatter(x, y, c=C, cmap = cmap)
     plt.show()
 
 
 if __name__ == "__main__":
-    # start time
+    # Intialize time
     start = time.time()
-    # get the temperature from the user
+    # Get input temperature from user
     temp = int(input("Enter the temperature: "))
     # call Function
-    main(temp)
+    state = main(temp)
 
-    
+    # Calculation end time
+    end = time.time()
+
+    time_taken = end - start
+    print(f"Total time taken to complete {time_taken} seconds")
+
+    # using list comprehension to create a list of all the values in the state
+    show_state = np.array([[state[i][j] for j in range(1, len(state) - 1)] for i in range(1, len(state) - 1)])
+
+    # Plotting the state
+    plot_state(show_state)
--- 28-97153-python/parallelHeatDiff.py
+++ 28-97153-python/parallelHeatDiff.py
@@ -1,77 +1,85 @@
-# Name: Venkata Vadrevu
-# FSU id: vv18d
+# importing packages
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.colors as col
+import matplotlib.colors as mcolors
 import time
-import multiprocessing as mp
+from multiprocessing import Pool
 import os
 
 
-def distribute_heat_col(y, old_grid):
+
+def main(temp):
+
+    # Creating halo cells & create as grid
+    state = np.array([[temp * 1.00 if i == 0 else 0.00 for i in range(temp + 2)] for j in range(temp + 2)])
+
+    num_iteration = 0
+    max_iteration_limit = 3000
+    cpu_cores = os.cpu_count() # this give no of cpu in the system
+    # Multiprocessing 
+    pool = Pool(processes=cpu_cores)
+
+    while num_iteration < max_iteration_limit:
+        state_1 = np.copy(state)
+        # calculating in async method
+        result = [pool.apply_async(calculate_temp, args=(i,state_1)) for i in range(1, temp+1)]
+        for j in result:
+            y, arr = j.get()
+            for i in range(1, temp+1):
+                state[i, y] = arr[i - 1]
+
+        if equals(state, state_1):
+            break
+        else:
+            num_iteration += 1
+
+    pool.close()
+
+    return state
+
+
+def calculate_temp(y, state_1):
     result = []
-    for x in range(1, len(old_grid) - 1):
-        result.append(0.25*(old_grid[x - 1, y] + old_grid[x + 1, y] + old_grid[x, y - 1] + old_grid[x, y + 1]))
+    for x in range(1, len(state_1) - 1):
+        result.append(0.25*(state_1[x - 1, y] + state_1[x + 1, y] + state_1[x, y - 1] + state_1[x, y + 1]))
     return y,result
 
 
-def equals(old_grid, grid):
-    for i in range(len(old_grid)):
-        for j in range(len(old_grid)):
-            if old_grid[i, j] != grid[i, j]:
+def equals(state_1, state):
+    for i in range(len(state_1)):
+        for j in range(len(state_1)):
+            if state_1[i, j] != state[i, j]:
                 return False
     return True
 
 
-def main():
 
-    # get the temperature from the user
-    temp = int(input("Enter the temperature: "))
-
-    # laminar metal
-    # created halo cells
-    grid = np.array([[temp * 1.00 if i == 0 else 0.00 for i in range(temp + 2)] for _ in range(temp + 2)])
-
-    num_iter = 0
-    MAX_ITER = 3000
-    num_cores = os.cpu_count()
-
-    pool = mp.Pool(processes=num_cores)
-
-    while num_iter < MAX_ITER:
-        old_grid = np.copy(grid)
-        result = [pool.apply_async(distribute_heat_col, args=(i,old_grid)) for i in range(1, temp+1)]
-        for p in result:
-            y, arr = p.get()
-            for i in range(1, temp+1):
-                grid[i, y] = arr[i - 1]
-
-        if equals(grid, old_grid):
-            break
-        else:
-            num_iter += 1
-
-    pool.close()
-
-    return grid
-
-
-def plot_grid(grid):
+def plot_state(state):
     fig, ax = plt.subplots()
-    y = np.array([[_ for __ in range(len(grid))] for _ in range(len(grid))])
-    x = np.array([[__ for __ in range(len(grid))] for _ in range(len(grid))])
-    cmap = col.ListedColormap(['darkblue', 'blue', 'aqua', 'lawngreen', 'yellow', 'orange', 'red', 'darkred'])
-    ax.scatter(x,y,c=grid, cmap=cmap)
+    y = np.array([[_ for __ in range(len(state))] for _ in range(len(state))]) # creating x axis
+    x = np.array([[__ for __ in range(len(state))] for _ in range(len(state))]) # creating y axis
+    C = state
+    cmap = mcolors.ListedColormap(['darkblue', 'blue', 'aqua', 'lawngreen', 'yellow', 'orange', 'red', 'darkred'])
+    ax.scatter(x, y, c=C, cmap = cmap)
     plt.show()
 
 
 if __name__ == "__main__":
-    # start time
+    # Intialize time
     start = time.time()
-    grid = main()
-    end = time.time()
-    print("Time taken ", end - start, "seconds")
+    # Get input temperature from user
+    temp = int(input("Enter the temperature: "))
+    # call Function
+    state = main(temp)
 
-    show_grid = np.array([[grid[i, j] for j in range(1, len(grid) - 1)] for i in range(1, len(grid) - 1)])
-    # Plot the grid
-    plot_grid(show_grid)
+    # Calculation end time
+    end = time.time()
+
+    time_taken = end - start
+    print(f"Total time taken to complete {time_taken} seconds")
+
+    # using list comprehension to create a list of all the values in the state
+    show_state = np.array([[state[i][j] for j in range(1, len(state) - 1)] for i in range(1, len(state) - 1)])
+
+    # Plotting the state
+    plot_state(show_state)
