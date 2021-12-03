8c35a4b6378e861bcbc8811ca37cdc86c7e335d8
dineshh <dinesh.kumar@avmsolutionsuk.com>
2021-12-03 12:16:38 +0000

    friend-recommender-added

--- 31-97850-friend-recommender/code/friend-recommender.py
+++ 31-97850-friend-recommender/code/friend-recommender.py
@@ -14,13 +14,8 @@ class SocialNetwork:
         Returns:
             [str]: A list of usernames
         '''
-        # saving list of users
-        list_of_users = []
-        # for each user in the network
-        for user in self.users:
-            # add the user to the list of users
-            list_of_users.append(user)
-        return list_of_users
+        # retutn  list of users in a list
+        return list(self.users.keys())
 
     def add_user(self, user):
         '''Add a user to the network
@@ -54,7 +49,8 @@ class SocialNetwork:
             None
         '''
         # add friends into the dict of user
-        self.users[user].append(friend)
+        if user in self.users:
+            self.users[user].append(friend)
 
     def get_friends(self, user):
         '''Get the friends of a user
@@ -66,9 +62,8 @@ class SocialNetwork:
             [str]: The list of usernames of the user's friends
 
         '''
-        # get friends of the user and save as a list
-        list_of_friends = self.users.get(user, [])
-        return list_of_friends
+        # get friends of the user and return as list
+        return self.users[user]
 
     def suggest_friend(self, user):
         '''Suggest a friend to the user
@@ -81,43 +76,59 @@ class SocialNetwork:
         Returns:
             str: The username of a new candidate friend for the user
         '''
-        # calculate the jaccard index
+        # this will store all the friends of the user because we can't suggest person who is already a friend
+        user_friends = [] 
+        # variable to store jaccard index
         jaccard_index = {}
-        dict_of_different_friends = {}
-        for others, others_friends in self.users.items():
-            if others != user:
-                common_friends = 0
-                total_people_met = 0
-                num_of_different_friends = 0
-                for other_friend in others_friends:
-                    if other_friend in self.users[user]:
-                        common_friends += 1
-                    else:
-                        num_of_different_friends += 1
-                    total_people_met += 1
-                dict_of_different_friends[others] = num_of_different_friends
-                total_people_met += len(self.users[user]) - common_friends
-                jaccard_index[others] = common_friends/total_people_met
+        # Below variables store count of the mutual and total friends
+        # which needed to calculate jaccard index
+        mutual_friends = total_friends = 0
 
-        # Get the user with high jaccard index
-        highest_ji = 0
-        potential_friends = []
-        for person, ji_value in jaccard_index.items():
-            if ji_value > highest_ji:
-                potential_friends = [person]
-                highest_ji = ji_value
-            elif ji_value == highest_ji:
-                potential_friends.append(person)
+        # get all the friends of the user and save it user_friends list
+        for friend in self.get_friends(user):
+            user_friends.append(friend)
 
-        #find person with highest number of different friends these highest jaccard people have with user
-        potential_friend = "No friends recommended for now!!"
-        highest_diff_friends = 0
-        for p_friend in potential_friends:
-            if dict_of_different_friends.get(p_friend, 0) > highest_diff_friends:
-                potential_friend = p_friend
-                highest_diff_friends = dict_of_different_friends.get(p_friend)
-        return potential_friend
+        # loop through the network
+        for person in self.users.keys():
+            # we already saved the friends of the user in user_friends list
+            if person != user:
+                different_person_friends = self.get_friends(person)
+            
+            # check the friends are mutual
+            for different_person in different_person_friends:
+                # checking the other person friends are matching with user friend
+                if different_person in user_friends:
+                    mutual_friends += 1
+                total_friends += 1
+            # calculate the jaccard index
+            total_friends += len(user_friends) # add user friends to total friends
+            jaccard_index[person] = mutual_friends / total_friends # adding jaccard index to dict
 
+        # find people with highest jaccard index
+        max_jaccard_index_person = max(jaccard_index, key=jaccard_index.get)
+
+        # Friends of highst jaccard index person are similar friend list
+        similar_friends = self.get_friends(max_jaccard_index_person)
+
+        suggested_friend = []
+        # from the similar friends list get suggestion of friends
+        for friend in similar_friends:
+            if friend not in user_friends and friend != user:
+                suggested_friend.append(friend)
+        
+        suggested_friend_dict = {}
+        # if the user is friends with everyone then we can't suggest anyone
+        if len(suggested_friend) == 0:
+            return "No Suggestion"
+        else:
+            # in the list of suggested friends and their friends count
+            # this step if more than one person to suggest then we will get the person with max friends count
+            for friend in suggested_friend:
+                suggested_friend_dict[friend] = len(self.get_friends(friend))
+
+        recommnation_friend = max(suggested_friend_dict, key=suggested_friend_dict.get)
+
+        return recommnation_friend
 
     def to_dot(self):
         result = []
@@ -154,8 +165,11 @@ def create_network_from_file(filename):
 def main():
     # network = create_network_from_file('simple.network')
     network = create_network_from_file('intermediate.network')
+    # network = create_network_from_file('twitter.network')
     print(network.to_dot())
-    print(network.suggest_friend('erin'))
+    print(network.suggest_friend('francis'))
+    # Francis not available in twitter network so tested with LAPhil
+    # rint(network.suggest_friend('LAPhil'))
 
 
 if __name__ == '__main__':
